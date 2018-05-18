161226eb6f3fe23b36e09bf87c2e90b53d39e1d6
Grace Shih <vocalise@gmail.com>
2018-05-18 19:59:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,13 +6,7 @@ class Triangle
   end
   
   def kind
-    if side1 == side2 && side1 == side3
-      :equilateral
-    elsif side1 == side2 or side2 == side3 or side1 == side3
-      :isosceles
-    elsif side1 != side2 and side2 != side3 and side1 != side3
-      :scalene
-    end
+    
   end
 
   class TriangleError < StandardError
