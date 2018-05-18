7ddc05da54d5b351836d896d270f998b01d69391
Grace Shih <vocalise@gmail.com>
2018-05-18 20:09:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,8 +2,10 @@ require 'pry'
 class Triangle
   attr_accessor :side1, :side2, :side3
   
-  def initialize(attributes)
-    attributes.each {|key,value| self.send(("#{key}="), value)}
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
   
   def kind
