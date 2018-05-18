3bc53ea989901e7016d5e23fd70fd5da525f1aa3
Grace Shih <vocalise@gmail.com>
2018-05-18 20:13:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,15 +9,23 @@ class Triangle
   end
   
   def kind
-    if @side1 == @side2 && @side2 == @side3
-      return :equilateral
-    elsif @side1 == @side3 or @side2 == @side3 or @side1 == @side2
-      return :isosceles
-    elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
-      return :scalene
-    end
+    if @side1 == 0 || @side2 == 0 || @side3 == 0
+      begin
+        raise TriangleError
+      rescue TriangleError =>error
+        puts error.message
+      end
+    else
+      if @side1 == @side2 && @side2 == @side3
+        return :equilateral
+      elsif @side1 == @side3 or @side2 == @side3 or @side1 == @side2
+        return :isosceles
+      elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+        return :scalene
+      end
   end
 
   class TriangleError < StandardError
+    
   end  
 end
