ad020566706fba21e5708cefa31f8474a4eae0e6
Grace Shih <vocalise@gmail.com>
2018-05-18 19:55:07 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   attr_accessor :side1, :side2, :side3
   
   def initialize(attributes)
-    attributes.each {|key,value| self.send(("#{key}="), value)
+    attributes.each {|key,value| self.send(("#{key}="), value)}
   end
   
   def kind
