fbb2faad5aafda49929f082b4d494fe8cfdb7d5f
Grace Shih <vocalise@gmail.com>
2018-05-18 20:14:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,11 +10,7 @@ class Triangle
   
   def kind
     if @side1 == 0 || @side2 == 0 || @side3 == 0
-      begin
-        raise TriangleError
-      rescue TriangleError =>error
-        puts error.message
-      end
+      raise TriangleError
     else
       if @side1 == @side2 && @side2 == @side3
         return :equilateral
